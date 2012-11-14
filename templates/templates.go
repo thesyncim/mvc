658d93ccff93174a7bd447a294f5b63e3c821b81
syncim <thesyncim@gmail.com>
2012-11-14 13:40:45 +0000

    mvc

---
+++ templates/templates.go
@@ -0,0 +1,30 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: syncim
+ * Date: 11/14/12
+ * Time: 12:59 PM
+ * To change this template use File | Settings | File Templates.
+ */
+package templates
+
+import (
+	"net/http"
+	"html/template"
+
+)
+
+
+type Entry struct {
+	Title	  string
+	Content	string
+}
+var templates = template.Must(template.ParseFiles("edit.html", "view.html"))
+
+func RenderTemplate(w http.ResponseWriter, tmpl string, p *Entry) {
+	err := templates.ExecuteTemplate(w, tmpl + ".html", p)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
+
+
