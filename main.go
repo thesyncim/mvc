af82fe881e24f7054fd4b3221cc0c69e3d945805
syncim <thesyncim@gmail.com>
2012-11-15 10:44:48 +0000

    mvc

---
+++ main.go
@@ -0,0 +1,23 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: syncim
+ * Date: 11/14/12
+ * Time: 12:57 PM
+ * To change this template use File | Settings | File Templates.
+ */
+package mvc
+
+import ("github.com/gorilla/mux"
+	"net/http"
+	"github.com/thesyncim/mvc/controllers"
+
+)
+
+func main() {
+	r := mux.NewRouter()
+	r.HandleFunc("/{teste}/{id}", controllers.HomeHandler)
+	http.Handle("/", r)
+	http.ListenAndServe(":8090", r)
+
+}
+
